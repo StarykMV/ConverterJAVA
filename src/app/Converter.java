65cf271e980e65a830d96afa259238b5c2a69fec
Miroslav <myroslav.staryk@viti.edu.ua>
2025-04-06 15:46:27 +0300

    Додано функцію конвертації кілометрів у милі

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -7,9 +7,17 @@ public class Converter {
         double miles = 10;
         double kilometers = milesToKilometers(miles);
         System.out.println(miles + " miles = " + kilometers + " kilometers");
+
+        double km = 16.0934;
+        double mi = kilometersToMiles(km);
+        System.out.println(km + " kilometers = " + mi + " miles");
     }
 
     public static double milesToKilometers(double miles) {
         return miles * 1.60934;
+    }
+
+    public static double kilometersToMiles(double kilometers) {
+        return kilometers / 1.60934;
     }
 }
