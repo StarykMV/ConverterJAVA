0aadeb79e4bcae5e730de64709b5abb8eae98413
Miroslav <myroslav.staryk@viti.edu.ua>
2025-04-06 15:34:37 +0300

    Додано конвертацію миль у кілометри

--- src/app/Converter.java
+++ src/app/Converter.java
@@ -3,5 +3,13 @@ package app;
 public class Converter {
     public static void main(String[] args) {
         System.out.println("Converter Project - Miles <-> Kilometers");
+
+        double miles = 10;
+        double kilometers = milesToKilometers(miles);
+        System.out.println(miles + " miles = " + kilometers + " kilometers");
+    }
+
+    public static double milesToKilometers(double miles) {
+        return miles * 1.60934;
     }
 }
